5c3e2865fec15b24e54433def9fd615b399dec56
MorganGravelat <mgravelat@gmail.com>
2023-01-11 11:52:59 -0500

    Change to init in app

--- app/__init__.py
+++ app/__init__.py
@@ -81,9 +81,11 @@ def inject_csrf_token(response):
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
-def react_root(path):
-    if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico')
+def catch_all(path):
     return app.send_static_file('index.html')
+# def react_root(path):
+#     if path == 'favicon.ico':
+#         return app.send_static_file('favicon.ico')
+#     return app.send_static_file('index.html')
 
 # push to heroku
