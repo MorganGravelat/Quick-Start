60dcb0cdb51d6794ea7ae81acd9742f44e6fe6f5
MorganGravelat <mgravelat@gmail.com>
2023-01-13 13:07:43 -0500

    Static File

--- app/__init__.py
+++ app/__init__.py
@@ -82,7 +82,7 @@ def inject_csrf_token(response):
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def catch_all(path):
-    return app.send_static_file('index.html')
+    return app.send_static_file('/react-app/public/index.html')
 # def react_root(path):
 #     if path == 'favicon.ico':
 #         return app.send_static_file('favicon.ico')
